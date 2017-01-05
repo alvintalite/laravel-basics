6db4695c60f3a4708e2af18f9aa4bbc0f759bdf5
alvintalite <alvintalite13@gmail.com>
2017-01-06 02:12:05 +0800

    final commit

--- routes/web.php
+++ routes/web.php
@@ -2,51 +2,78 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Application Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
 |
 */
 
-Route::group(['middleware' => ['web']], function(){
+
+
+Route::group(['middlewareGroups' => ['web']], function() {
 	Route::get('/', function () {
-	    return view('welcome');
-	})->name('home');
-	Route::post('/signup',[
-		'uses' => 'UserController@postSignUp',
-		'as' => 'signup'
-	]);
+    return view('welcome');
+})->name('home');
 
-	Route::post('/signin', [
-		'uses' => 'UserController@postSignin',
-		'as' => 'signin'
-	]);
+Route::post('/signup', [
+    'uses' => 'UserController@postSignUp',
+    'as' => 'signup'
+]);
 
-	Route::get('/logout', [
-		'uses'=>'UserController@getLogout',
-		'as'=>'logout'
+Route::post('/signin', [
+    'uses' => 'UserController@postSignIn',
+    'as' => 'signin'
+]);
 
-		]);
+Route::get('/logout', [
+    'uses' => 'UserController@getLogout',
+    'as' => 'logout'
+]);
 
-	Route::get('/dashboard',[
-		'uses' => 'PostController@getDashboard',
-		'as' => 'dashboard',
-		'middleware'=>'auth'
-	]);
+Route::get('/account', [
+    'uses' => 'UserController@getAccount',
+    'as' => 'account'
+]);
 
-		Route::post('/createpost', [
-			'uses' =>'PostController@postCreatePost',
-			'as'=>'post.create',
-			'middleware'=>'auth'
-			]);
+Route::post('/upateaccount', [
+    'uses' => 'UserController@postSaveAccount',
+    'as' => 'account.save'
+]);
 
-	Route::get('/delete-post/{post_id}',[
-		'uses'=>'PostController@getDeletePost',
-		'as'=>'post.delete',
-		'middleware'=>'auth'
-		
-		]);
-});
+Route::get('/userimage/{filename}', [
+    'uses' => 'UserController@getUserImage',
+    'as' => 'account.image'
+]);
+
+Route::get('/dashboard', [
+    'uses' => 'PostController@getDashboard',
+    'as' => 'dashboard',
+    'middleware' => 'auth'
+]);
+
+Route::post('/createpost', [
+    'uses' => 'PostController@postCreatePost',
+    'as' => 'post.create',
+    'middleware' => 'auth'
+]);
+
+Route::get('/delete-post/{post_id}', [
+    'uses' => 'PostController@getDeletePost',
+    'as' => 'post.delete',
+    'middleware' => 'auth'
+]);
+
+Route::post('/edit', [
+    'uses' => 'PostController@postEditPost',
+    'as' => 'edit'
+]);
+
+Route::post('/like', [
+    'uses' => 'PostController@postLikePost',
+    'as' => 'like'
+]);
+
+}); 
